e76d2185a1b50ba26726a61a6b07339e3146ec12
leo <3181538941@qq.com>
2023-05-08 12:10:48 +0800

    init

---
+++ captchaGenerator.py
@@ -0,0 +1,38 @@
+# -*- coding: UTF-8 -*-
+from captcha.image import ImageCaptcha  # pip install captcha
+from PIL import Image
+import random
+import time
+import settings
+import os
+
+
+def random_captcha():
+    captcha_text = []
+    for i in range(settings.MAX_CAPTCHA):
+        c = random.choice(settings.ALL_CHAR_SET)
+        captcha_text.append(c)
+    return ''.join(captcha_text)
+
+
+# 生成字符对应的验证码
+def gen_captcha_text_and_image():
+    image = ImageCaptcha()
+    captcha_text = random_captcha()
+    captcha_image = Image.open(image.generate(captcha_text))
+    return captcha_text, captcha_image
+
+
+if __name__ == '__main__':
+    count = 10000
+    path = settings.TRAIN_DATASET_PATH  # 通过改变此处目录，以生成 训练、测试和预测用的验证码集
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    for i in range(count):
+        now = str(int(time.time()))
+        text, image = gen_captcha_text_and_image()
+        filename = text + '_' + now + '.jpg'
+        image.save(path + os.path.sep + filename)
+        if i % (count / 20) == 0:
+            print('saved %d : %s' % (i + 1, filename))
